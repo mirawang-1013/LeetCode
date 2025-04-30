4651113d6e90512bfe2979775dc6d8bdbd246780
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-04-30 16:09:18 +0800

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -5,12 +5,12 @@ class Solution:
         for num in nums:
             if num-1 not in num_set: #try to find out the starting point
                 current = num
-                length = 1
+                count = 1
 
                 while current+1 in num_set:
                     current += 1
-                    length += 1
+                    count += 1
                 
-                longest = max(longest, length)
+                longest = max(longest, count)
 
         return longest
