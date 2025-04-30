bff5988300fa5e6dc6ea202515de612cd8c1333e
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-04-30 23:04:09 +0800

    LC-missing-number [Runtime: 2 ms; Memory: 19.1 MB]

--- sorting/missing-number.py
+++ sorting/missing-number.py
@@ -1,9 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        total = n*(n+1)//2
-        actual = sum(nums)
-        missing = total-actual
+        num_set = set(nums)
+        for i in range(len(nums)+1):
+            if i not in num_set:
+                return i
+        # math method
+        # n = len(nums)
+        # total = n*(n+1)//2
+        # actual = sum(nums)
+        # missing = total-actual
 
-        return missing
+        # return missing
         
