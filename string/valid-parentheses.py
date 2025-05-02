9d9fbd9f6e55e644c1100af25eddff0ee0a2c177
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-05-02 22:11:13 +0800

    LC-valid-parentheses [Runtime: 2 ms; Memory: 17.7 MB]

---
+++ string/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs={')':'(',']':'[','}':'{'}
+        stack = []
+        array=()
+        
+        for char in s:
+            if char in '([{':
+                stack.append(char)
+            elif char in ')]}':
+                if not stack or stack.pop() != pairs[char]:
+                    return False      
+        return not stack
+
+# if len(stack) == 0:
+#     return True
+# else:
+#     return False
+
+
+       
