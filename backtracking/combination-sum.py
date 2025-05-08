3f2996b3b15283fc04615d207098251cdfd52026
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-05-09 00:15:01 +0800

    LC-combination-sum [Runtime: 7 ms; Memory: 17.8 MB]

---
+++ backtracking/combination-sum.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, path, target):
+            if target == 0:
+                res.append(path[:])
+                return 
+
+            if target<0:
+                return 
+            
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, target - candidates[i])
+                path.pop()
+
+        backtrack(0, [], target)
+        return res
