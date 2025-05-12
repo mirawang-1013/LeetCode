d42445cd8bdd223729edcf38489afbb23b3f5df3
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-05-12 11:21:37 +0800

    LC-maximum-depth-of-binary-tree [Runtime: 0 ms; Memory: 18.8 MB]

---
+++ tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    #maxDepth()这个函数就是返回节点的深度（但是不包括root这个深度所以最后要加1）
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return max(left_depth, right_depth) + 1
+        
