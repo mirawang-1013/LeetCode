56576aa8d4da7e4c857522dffe8db9452abeb7c1
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-05-13 09:38:10 +0800

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -7,9 +7,9 @@ class Solution:
             if num-1 not in num_set:
                 current = num
                 count = 1
-            while current+1 in num_set:
-                current += 1
-                count += 1
+                while current+1 in num_set:
+                    current += 1
+                    count += 1
             
             longest = max(longest, count)
         return longest
