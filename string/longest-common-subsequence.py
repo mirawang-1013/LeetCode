e3c4d1fba9635da18fd6d8d8bebf855c8058bef6
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-05-27 09:37:07 +0800

    LC-longest-common-subsequence [Runtime: 459 ms; Memory: 42.8 MB]

--- string/longest-common-subsequence.py
+++ string/longest-common-subsequence.py
@@ -1,16 +1,16 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        p1,p2=0,0
-        length=0
+        m,n = len(text1),len(text2)
 
-        while p1<len(text1) and p2<len(text2):
-            if text1[p1]==text2[p2]:
-                length+=1
-                p1+=1
-                p2+=1
-            else:
-                p1+=1
-        return length
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[m][n]
 
 
         
