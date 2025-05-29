bbc8bff39c8061de2f45f49697bb252e4eba2e45
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-05-29 11:04:17 +0800

    LC-merge-k-sorted-lists [Runtime: 7 ms; Memory: 20.1 MB]

--- heap-priority-queue/merge-k-sorted-lists.py
+++ heap-priority-queue/merge-k-sorted-lists.py
@@ -1,31 +1,40 @@
 import heapq
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-
-
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        min_heap = []
-        count = 0  # 保证每个元素唯一
+        #如果list什么也没有就返回None
+        if not lists or len(lists) == 0:
+            return None
+        #当list有内容的时候，创建一个stack来暂时保存两两合并的新链表
+        while len(lists) > 1:
+            merge=[]
+        #
+            for i in range(0,len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1)<len(lists) else None
+                #每两个链表之间合并
+                merge.append(self.mergeList(l1,l2))
+            lists = merge
+        return lists[0]
 
-        for l in lists:
-            if l:
-                heapq.heappush(min_heap, (l.val, count, l))
-                count += 1
+    def mergeList(self,l1,l2):
+        dummy = ListNode()
+        tail = dummy
 
-        dummy = ListNode(0)
-        current = dummy
-
-        while min_heap:
-            val, _, node = heapq.heappop(min_heap)
-            current.next = node
-            current = current.next
-            if node.next:
-                heapq.heappush(min_heap, (node.next.val, count, node.next))
-                count += 1
-
+        while l1 and l2:
+            if l1.val<l2.val:
+                tail.next=l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
         return dummy.next
