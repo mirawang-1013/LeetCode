0a5922ad6ebef37e17c0c6aee7796e674ea8305b
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-04 16:06:02 +0800

    LC-valid-anagram [Runtime: 3 ms; Memory: 17.8 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,17 +1,24 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        pair1=[]
-        pair2=[]
-        s=sorted(s)
-        t=sorted(t)
-        if len(s) != len(t):
-            return False
-        for i in s:
-            pair1.append(i)
-        for j in t:
-            pair2.append(j)
-        if pair1 == pair2:
-            return True
-        else: 
-            return False
+    #Solution 1
+        return Counter(s) == Counter(t)
+
+
+
+   #Solution 2     
+        # pair1=[]
+        # pair2=[]
+        # s=sorted(s)#don't put the sorted function in loop, too slow
+        # t=sorted(t)
+        # if len(s) != len(t):
+        #     return False
+        # for i in s:
+        #     pair1.append(i)
+        # for j in t:
+        #     pair2.append(j)
+        # if pair1 == pair2:
+        #     return True
+        # else: 
+        #     return False
         
