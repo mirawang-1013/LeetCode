c0c512cb1adba52bcf65095d1c55e992b1f84a34
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-04 16:35:38 +0800

    LC-valid-palindrome [Runtime: 7 ms; Memory: 23.7 MB]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,20 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l,r = 0, len(s)-1
+        filtered = [c.lower() for c in s if c.isalnum()]
+        return filtered == filtered[::-1]
 
-        while l<r:
-            if not self.isAlNum(s[l]):
-                l+=1
-                continue
+
+
+    #Solution
+        # l,r = 0, len(s)-1
+
+        # while l<r:
+        #     if not self.isAlNum(s[l]):
+        #         l+=1
+        #         continue
                
-            if not self.isAlNum(s[r]):
-                r-=1
-                continue
+        #     if not self.isAlNum(s[r]):
+        #         r-=1
+        #         continue
          
-            if s[l].lower() != s[r].lower():
-                return False
-            l,r = l+1, r-1
-        return True
+        #     if s[l].lower() != s[r].lower():
+        #         return False
+        #     l,r = l+1, r-1
+        # return True
 
 
     def isAlNum(self,c):
