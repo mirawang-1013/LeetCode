7ef7b09bd22b0a9e73e31f2760cae1b17ba5d82d
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-07 19:34:23 +0800

    LC-generate-parentheses [Runtime: 0 ms; Memory: 18.2 MB]

--- backtracking/generate-parentheses.py
+++ backtracking/generate-parentheses.py
@@ -1,7 +1,9 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        #step1:定义结果stack
         res = []
-
+        #n是有多少对括号的意思
+         #step2:定义backtrack回溯函数
         def backtrack(path:str, open_count:int, close_count:int):
             #结束的条件是左括号和右括号都用完了
             if open_count == n and close_count == n:
@@ -13,6 +15,7 @@ class Solution:
              # 右括号必须比左括号少才能放
             if close_count<open_count:
                 backtrack(path+')',open_count,close_count+1)
-
+        
+        #step3:初始化
         backtrack("",0,0)        
         return res
