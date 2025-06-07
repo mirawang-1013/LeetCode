4b4b00a0ef4d5816b2137f2641e357b8f8a5c46a
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-07 17:29:42 +0800

    LC-valid-palindrome [Runtime: 3 ms; Memory: 23.7 MB]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,7 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered = [c.lower() for c in s if c.isalnum()]
-        return filtered == filtered[::-1]
+       filtered = [c.lower() for c in s if c.isalnum()]
+       return filtered  == filtered[::-1]
+
+
+
+
+
+
 
 
     #Solution
