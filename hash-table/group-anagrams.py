4b54e5525ec538a4ffcfb591ce90a2787689dc21
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-07 19:54:04 +0800

    LC-group-anagrams [Runtime: 11 ms; Memory: 20.6 MB]

--- hash-table/group-anagrams.py
+++ hash-table/group-anagrams.py
@@ -3,7 +3,10 @@ class Solution:
         anagram_map = defaultdict(list)
 
         for word in strs:
+            #相当于把所有的strs都按照顺序分组
             sorted_word = ''.join(sorted(word))
             anagram_map[sorted_word].append(word)
 
-        return list(anagram_map.values())        
+        return list(anagram_map.values())       
+
+
