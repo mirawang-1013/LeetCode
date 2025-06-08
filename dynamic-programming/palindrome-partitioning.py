b3875f00542f5cd9da8c5241afa8679e590e5cb5
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-08 18:49:18 +0800

    LC-palindrome-partitioning [Runtime: 121 ms; Memory: 33.6 MB]

---
+++ dynamic-programming/palindrome-partitioning.py
@@ -0,0 +1,33 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        #answer
+        res = []
+        #store path
+        path = []
+
+        #判断是否是回文
+        def is_palindrome(sub):
+            #正着数=倒着数
+            return sub==sub[::-1]
+        
+        #回溯
+        def backtrack(start):
+            #当开始的点等于s的长度
+            if start == len(s):
+                print('start == len(s)',start,'==',len(s)
+                )
+                #加入路径
+                res.append(path[:])
+                print('path[:]')
+                return 
+                #当确定了start的点，开始loop start后面的每个字符，一直到s的最后一个字符
+            for end in range(start+1, len(s)+1):
+                #哪一节是回文
+                if is_palindrome(s[start:end]):
+                    #如果是回文，就把这个字段放进去
+                    path.append(s[start:end])
+                    #然后以字段结束为起点，再继续找下一个回文字符串
+                    backtrack(end)
+                    path.pop()
+        backtrack(0)
+        return res
