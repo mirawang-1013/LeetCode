a422efd2ccd82d8e65a91f191042fb9912b18577
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-08 23:29:03 +0800

    LC-roman-to-integer [Runtime: 4 ms; Memory: 17.7 MB]

--- hash-table/roman-to-integer.py
+++ hash-table/roman-to-integer.py
@@ -1,19 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman={
+        romans = {
             "I":1,
             "V":5,
             "X":10,
             "L":50,
             "C":100,
             "D":500,
-            "M":1000
-        }
-
-        total = 0
+            "M":1000}
+        total=0
         for i in range(len(s)):
-            if i+1<len(s) and roman[s[i]]<roman[s[i+1]]:
-                total -= roman[s[i]]
+            if i+1<len(s) and romans[s[i]]<romans[s[i+1]]:
+                total -= romans[s[i]]
             else:
-                total += roman[s[i]]
+                total += romans[s[i]]
+    
         return total
