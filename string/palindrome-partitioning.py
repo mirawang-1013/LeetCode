3f7d896d44daee9c1147d093e1f1dbdabb809f8b
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-08 18:56:26 +0800

    LC-palindrome-partitioning [Runtime: 48 ms; Memory: 34.5 MB]

--- string/palindrome-partitioning.py
+++ string/palindrome-partitioning.py
@@ -8,21 +8,19 @@ class Solution:
         #判断是否是回文
         def is_palindrome(sub):
             #正着数=倒着数
-            return sub==sub[::-1]
+            return sub == sub[::-1]
         
         #回溯
         def backtrack(start):
             #当开始的点等于s的长度
             if start == len(s):
-                print('start == len(s)',start,'==',len(s)
-                )
                 #加入路径
                 res.append(path[:])
-                print('path[:]')
-                return 
+                return
+
                 #当确定了start的点，开始loop start后面的每个字符，一直到s的最后一个字符
-            for end in range(start+1, len(s)+1):
-                #哪一节是回文
+            for end in range(start+1,len(s)+1):
+                #哪一节是回文,start是固定的，end是顺着range一个一个往下推
                 if is_palindrome(s[start:end]):
                     #如果是回文，就把这个字段放进去
                     path.append(s[start:end])
