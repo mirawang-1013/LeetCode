9b8b9233149d5121825e6145c73aaf08b34658a7
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-09 00:24:36 +0800

    LC-generate-parentheses [Runtime: 0 ms; Memory: 17.8 MB]

--- backtracking/generate-parentheses.py
+++ backtracking/generate-parentheses.py
@@ -1,21 +1,57 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        #step1:定义结果stack
-        res = []
-        #n是有多少对括号的意思
-         #step2:定义backtrack回溯函数
-        def backtrack(path:str, open_count:int, close_count:int):
-            #结束的条件是左括号和右括号都用完了
-            if open_count == n and close_count == n:
+        res=[]
+
+        def backtrack(path,open_count,close_count):
+            if open_count==n and close_count==n:
                 res.append(path)
-                return 
+                return
             
             if open_count<n:
                 backtrack(path+'(',open_count+1,close_count)
-             # 右括号必须比左括号少才能放
             if close_count<open_count:
                 backtrack(path+')',open_count,close_count+1)
-        
-        #step3:初始化
-        backtrack("",0,0)        
+    
+        backtrack("",0,0)
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # #step1:定义结果stack
+        # res = []
+        # #n是有多少对括号的意思
+        #  #step2:定义backtrack回溯函数
+        # def backtrack(path:str, open_count:int, close_count:int):
+        #     #结束的条件是左括号和右括号都用完了
+        #     if open_count == n and close_count == n:
+        #         res.append(path)
+        #         return 
+            
+        #     if open_count<n:
+        #         backtrack(path+'(',open_count+1,close_count)
+        #      # 右括号必须比左括号少才能放
+        #     if close_count<open_count:
+        #         backtrack(path+')',open_count,close_count+1)
+        
+        # #step3:初始化
+        # backtrack("",0,0)        
+        # return res
