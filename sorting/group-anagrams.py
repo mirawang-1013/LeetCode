2f0466e0c3f09c897807c9ccd23dbedc883ff848
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-09 00:27:19 +0800

    LC-group-anagrams [Runtime: 8 ms; Memory: 20.6 MB]

--- sorting/group-anagrams.py
+++ sorting/group-anagrams.py
@@ -1,12 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = defaultdict(list)
-
-        for word in strs:
-            #相当于把所有的strs都按照顺序分组
-            sorted_word = ''.join(sorted(word))
-            anagram_map[sorted_word].append(word)
-
-        return list(anagram_map.values())       
+        group_anagrams = defaultdict(list)
+        for i in strs:
+            terms = ''.join(sorted(i))
+            group_anagrams[terms].append(i)
+        return list(group_anagrams.values())
+        
 
 
