147a283e89e5c4432f2dfce2384796e5a3a1e086
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-09 23:51:35 +0800

    LC-jump-game [Runtime: 41 ms; Memory: 18.7 MB]

--- array/jump-game.py
+++ array/jump-game.py
@@ -2,6 +2,8 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         max_reach = 0
         for i, jump in enumerate(nums):
+            #i是下标，jump是数值
+            #i+jump是最大的能覆盖的index
             if i > max_reach:
                 return False
             max_reach = max(max_reach, i+jump)
