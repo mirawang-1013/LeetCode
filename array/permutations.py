644015a8925857c3da22d24243de5a2c5899ba85
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-09 19:53:14 +0800

    LC-permutations [Runtime: 2 ms; Memory: 18.1 MB]

--- array/permutations.py
+++ array/permutations.py
@@ -2,14 +2,13 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         if len(nums)==1:
             return [[nums[0]]]
-        ans = []
+        ans=[]
         for i in range(len(nums)):
             first = [nums[i]]
             others = nums[:i]+nums[i+1:]
-            tmp= self.permute(others)
-            for x in tmp:
-                ans.append(first+x)
+            temp = self.permute(others)
+            for j in temp:
+                ans.append(first+j)
         return ans
-            
 
         
