77eff821d1c1398776114993c89109ad20d8b555
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-11 22:30:47 +0800

    LC-reverse-linked-list [Runtime: 0 ms; Memory: 18.9 MB]

--- recursion/reverse-linked-list.py
+++ recursion/reverse-linked-list.py
@@ -9,13 +9,54 @@ class Solution:
         curr = head
         while curr:
             stack.append(curr)
-            curr = curr.next
-        
+            curr=curr.next
         if not stack:
             return None
         
         new_head = stack.pop()
         curr = new_head
+        while stack:
+            curr.next = stack.pop()
+            curr = curr.next
+            
+        curr.next = None
+        return new_head
+            
+       
+    
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        #创建一个栈来存储数据
+        stack = []
+        #当前的节点是头
+        curr = head
+        #给栈里加节点
+        while curr:
+            stack.append(curr)
+            #让当前箭头指向下一个节点
+            curr = curr.next
+        
+        if not stack:
+            return None
+        
+        #新的list是从stack的最后一个pop出来
+        new_head = stack.pop()
+        #现在节点 = 最后一个pop出来的
+        curr = new_head
+        #当stack里还有数的时候，下一个node等于stack里的下一个数
         while stack:
             curr.next = stack.pop()
             curr = curr.next
