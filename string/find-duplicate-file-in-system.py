4c5b63403ff01939c5a7aaacab5997428e5793b7
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2025-06-13 16:38:15 +0800

    LC-find-duplicate-file-in-system [Runtime: N/A; Memory: N/A]

---
+++ string/find-duplicate-file-in-system.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        # if isinstance(paths, builtins.str):
+        #     paths = [paths]
+        
+        content_map = defaultdict(list)
+
+        for info in paths:
+          parts = info.split()  
+          dir_path = parts[0]
+          for fp in parts[1:]:
+             if '(' not in fp or not fp.endswith(')'):
+                    continue   
+             name, content = fp.split('(', 1)
+             content = content[:-1] 
+             content_map[content].append(f"{dir_path}/{name}")
+        result = list(content_map.values())
+        return result
