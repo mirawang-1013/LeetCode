bc1c750f6cb33ea58815283c9baeaa7fb47578dd
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-18 20:52:51 +0800

    LC-two-sum [Runtime: 450 ms; Memory: 19.7 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i,value in enumerate(nums):
             gap=target-value
-            if gap in nums and gap!= target:
+            if gap in nums:
                 j = nums.index(gap)
                 if i!=j:
                     return [i,j]
