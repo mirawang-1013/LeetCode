ffaceb867e68c854221d52a72ef03266bd566ea7
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-19 09:43:47 +0800

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- string/longest-palindromic-substring.py
+++ string/longest-palindromic-substring.py
@@ -1,58 +1,56 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res=""
-        resLen=0
+            result=""
+            def expand(l,r):
+                while l>0 and r<len(s) and s[l]==s[r]:
+                    l-=1
+                    r+=1
+                return s[l+1:r]
+            for i in range(len(s)):
+                odd=expand(i,i)
+                even = expand(i,i+1)
+                if len(odd)>len(result):
+                    result=odd
+                if len(even)>len(result):
+                    result = even
+            return result
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # res = ""
+        # resLen = 0
 
-        def expandAround(l:int,r:int):
-            nonlocal res, resLen
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if r-l+1>resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                r+=1
-                l-=1
-        for i in range(len(s)):
-            expandAround(i,i)
-            expandAround(i,i+1)
-        return res
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        res = ""
-        resLen = 0
-
-        def expandAroundCentre(l:int,r:int):
-            nonlocal res, resLen
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1)>=resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                r+=1
-                l-=1
-        for i in range(len(s)):
-            expandAroundCentre(i,i)
-            expandAroundCentre(i,i+1)
-        return res
+        # def expandAroundCentre(l:int,r:int):
+        #     nonlocal res, resLen
+        #     while l>=0 and r<len(s) and s[l]==s[r]:
+        #         if (r-l+1)>=resLen:
+        #             res = s[l:r+1]
+        #             resLen = r-l+1
+        #         r+=1
+        #         l-=1
+        # for i in range(len(s)):
+        #     expandAroundCentre(i,i)
+        #     expandAroundCentre(i,i+1)
+        # return res
 
 
 
