851272dc34cb9577a7c31b5cfa169eefb9dcfdcf
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-19 09:44:30 +0800

    LC-longest-palindromic-substring [Runtime: 252 ms; Memory: 19.1 MB]

--- two-pointers/longest-palindromic-substring.py
+++ two-pointers/longest-palindromic-substring.py
@@ -2,7 +2,7 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
             result=""
             def expand(l,r):
-                while l>0 and r<len(s) and s[l]==s[r]:
+                while l>=0 and r<len(s) and s[l]==s[r]:
                     l-=1
                     r+=1
                 return s[l+1:r]
