cb80c80a6925daf06f89e087ec3108d63ac9d5f1
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-21 14:11:34 +0800

    LC-trapping-rain-water [Runtime: 51 ms; Memory: 21.3 MB]

--- array/trapping-rain-water.py
+++ array/trapping-rain-water.py
@@ -1,59 +1,29 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
+        left,right=0,0
+        left_max=[]
+        right_max=[]
+        min_list=[]
+        drop=[]
+        for i in range(len(height)):
+            left=max(left,height[i])
+            left_max.append(left)
 
-        left,right = 0,len(height)-1
-        left_max,right_max = 0,0
-        water_trapped = 0
-        while left<right:
-            for i in range(len(height)):
-                if height[left]<height[right]:
-                    if height[left]>=left_max:
-                        left_max=height[left]
-                    else:
-                        water_trapped = water_trapped+left_max-height[left]
-                    left+=1
-                
-                else:
-                    if height[right]>right_max:
-                        right_max=height[right]
-                        
-                    else:
-                        water_trapped = water_trapped+right_max-height[right]
-                    right-=1
-        return water_trapped
-
+        for i in range(len(height)-1,-1,-1):
+            right=max(right,height[i])
+            right_max.append(right)
+        for i,j in zip(left_max,right_max[::-1]):
+            print(i,j)
+            min_list.append(min(i,j))
         
+        for i,j in zip(min_list,height):
+          drop.append(i-j)
+        return sum(drop)
 
 
 
-
-        # if not height:
-        #     return 0
-        
-        # left, right = 0, len(height)-1 #初始化左右两个指针
-        # left_max,right_max = 0,0  # 记录左右两边的最大高度
-        # water_trapped = 0
-
-        # while left<right: #左指针和右指针还没有相遇的时候
-        # #若左指针的高度小于右指针的高度
-        #     if height[left]<height[right]:
-        #         if height[left]>=left_max:
-        #             #如果这个height是最高的，那么left_max的值要更新一下
-        #             left_max = height[left]
-        #         else:
-        #             water_trapped += left_max-height[left]
-        #         left+=1
-        #     else:
-        #         if height[right]>=right_max:
-        #             right_max=height[right]
-        #         else:
-        #             water_trapped += right_max-height[right]
-        #         right -=1
-        # return water_trapped
-            
-            
+    
+                   
                 
 
 
