1e40bf1b7811e77416753ab07f5e2c12eed56042
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-21 12:13:22 +0800

    LC-3sum [Runtime: 628 ms; Memory: 22.4 MB]

--- array/3sum.py
+++ array/3sum.py
@@ -22,23 +22,23 @@ class Solution:
                     left+=1
                     right-=1
         return result       
-        # result=set()
-        # nums.sort()
-        # print(nums)
-        # left,right=0,len(nums)-1
-        # for i,value in enumerate(nums):
-        #     left,right=i+1,len(nums)-1
-        #     while left<right:
-        #         total=nums[i]+nums[left]+nums[right]
-        #         if total<0:
-        #             left+=1
-        #         if total>0:
-        #             right-=1
-        #         if total==0:
-        #             result.add(tuple([value,nums[left],nums[right]]))
-        #             left+=1
-        #             right-=1
-        # return [list(t) for t in result]
+        result=set()
+        nums.sort()
+        print(nums)
+        left,right=0,len(nums)-1
+        for i,value in enumerate(nums):
+            left,right=i+1,len(nums)-1
+            while left<right:
+                total=nums[i]+nums[left]+nums[right]
+                if total<0:
+                    left+=1
+                if total>0:
+                    right-=1
+                if total==0:
+                    result.add(tuple([value,nums[left],nums[right]]))
+                    left+=1
+                    right-=1
+        return [list(t) for t in result]
 
 
         
