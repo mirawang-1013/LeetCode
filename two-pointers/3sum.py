b4d58f43b0cae42e80cf64927f5834799b67b7ce
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-21 11:48:03 +0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -1,26 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        result=set()
         nums.sort()
+        print(nums)
+        left,right=0,len(nums)-1
+        for i,value in enumerate(nums):
+            left,right=i+1,len(nums)-1
+            while left<right:
+                total=nums[i]+nums[left]+nums[right]
+                print(f'total={i}+{nums[left]}+{nums[right]}')
+                print('total',total)
+                if total<0:
+                    left+=1
+                if total>0:
+                    right-=1
+                if total==0:
+                    result.add(tuple([value,nums[left],nums[right]]))
+                    left+=1
+                    right-=1
+        return [list(t) for t in result]
 
-        for i, a in enumerate(nums):
-            #skip duplicate to avoid same results
-            if i>0 and a==nums[i-1]:
-                continue
-            
-            l, r = i+1, len(nums)-1
-            while l<r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum<0:
-                    l += 1
-                else:
-                    result.append([a,nums[l],nums[r]])
-                    l += 1
-                    while nums[l] == nums[l-1] and l<r:
-                        l+=1
+
         
-        return result
-
         
