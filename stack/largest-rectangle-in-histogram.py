d118dba8d27494c817a1780772aa1d1e1cf41897
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-23 00:28:30 +0800

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- stack/largest-rectangle-in-histogram.py
+++ stack/largest-rectangle-in-histogram.py
@@ -3,17 +3,13 @@ class Solution:
         #很像接雨水的题目，但是接雨水的是最大，减最小的，得到能接的
         #这个像是临界的，但是取最短的，然后算面积
         max_area=0
-        area=0
+        area=1*heights[0]
         for i in range(len(heights)):
             w=i+1
-            if len(heights)>1:
-
-                while w<len(heights) and heights[w]>=heights[i]:
-                    area=(w-i+1)*heights[i]
-                    w+=1
-                    max_area=max(max_area,area)
-            else:
-                 max_area=1*heights[i]
+            while w<len(heights) and heights[w]>=heights[i]:
+                area=(w-i+1)*heights[i]
+                w+=1
+                max_area=max(max_area,area)
                
         return max_area
         
