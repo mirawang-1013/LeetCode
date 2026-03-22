fdbb76a01baf61f0469b177cd783d5f052e37119
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-23 00:32:30 +0800

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- array/largest-rectangle-in-histogram.py
+++ array/largest-rectangle-in-histogram.py
@@ -2,8 +2,8 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         #很像接雨水的题目，但是接雨水的是最大，减最小的，得到能接的
         #这个像是临界的，但是取最短的，然后算面积
-        max_area=0
         area=1*heights[0]
+        max_area=area
         for i in range(len(heights)):
             w=i+1
             if len(heights)>1:
