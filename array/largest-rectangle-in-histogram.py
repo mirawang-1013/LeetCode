6a0f52241a174a05f5b9795e80fa9ffadecb5f93
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-23 00:03:53 +0800

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- array/largest-rectangle-in-histogram.py
+++ array/largest-rectangle-in-histogram.py
@@ -6,9 +6,12 @@ class Solution:
         area=0
         for i in range(len(heights)):
             w=i+1
-            while w<len(heights) and heights[w]>heights[i]:
-                area=(w-i+1)*heights[i]
-                w+=1
-                max_area=max(max_area,area)
+            if len(heights)>1:
+                while w<len(heights) and heights[w]>heights[i]:
+                    area=(w-i+1)*heights[i]
+                    w+=1
+                    max_area=max(max_area,area)
+            else:
+                max_area=1*heights[i]
         return max_area
         
