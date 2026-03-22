5db2c6bb3643efa5e9176726d92f6259b3daa66c
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-23 00:18:18 +0800

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- monotonic-stack/largest-rectangle-in-histogram.py
+++ monotonic-stack/largest-rectangle-in-histogram.py
@@ -6,13 +6,13 @@ class Solution:
         area=0
         for i in range(len(heights)):
             w=i+1
-            if len(heights)==1:
-                 max_area=1*heights[i]
-            else:
+            if len(heights)>1:
                 while w<len(heights) and heights[w]>=heights[i]:
                         area=(w-i+1)*heights[i]
                         w+=1
                         max_area=max(max_area,area)
+            else:
+                 max_area=1*heights[i]
                
         return max_area
         
