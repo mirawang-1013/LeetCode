7fe9c8c33d7809502fd6408f34aea42cfa78cc28
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-26 00:10:01 +0800

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/search-in-rotated-sorted-array.py
+++ binary-search/search-in-rotated-sorted-array.py
@@ -1,23 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1
-
-        while left<=right:
-            #确定中点的位置
-            mid = (left+right)//2
-
-            if nums[mid] == target:
-                return mid
-            #如果左边是升序，就用二分法找
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target <nums[mid]:
-                    right = mid -1
-                else:
-                    left = mid+1
-            #如果右边是升序同样的方法找
+        left, right=0, len(nums)-1
+        if target not in nums:
+            return -1
+        while left<right:
+            mid = (left+right)//2   
+            if nums[mid]>target and nums[mid]>nums[right]:
+                left=mid+1
             else:
-                if nums[mid]<target<=nums[right]:
-                    left = mid+1
-                else:
-                    right = mid-1
-        return -1
+                right=mid
+        return left
+            
