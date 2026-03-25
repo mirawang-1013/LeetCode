b214d87b2eaa536dd015e5ec9e298247b8a4698f
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-25 13:24:08 +0800

    LC-trapping-rain-water [Runtime: 15 ms; Memory: 21.1 MB]

--- two-pointers/trapping-rain-water.py
+++ two-pointers/trapping-rain-water.py
@@ -1,20 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left,right=0,0
-        left_max=[]
-        right_max=[]
-        min_max=[]
+        left=0
+        right=0
+        left_max=[0]*len(height)
+        right_max=[0]*len(height)
+        min_max=[0]*len(height)
         drop=0
         for i in range(len(height)):
             left=max(left,height[i])
-            left_max.append(left)
+            left_max[i]=left
         for i in range(len(height)-1,-1,-1):
             right=max(right,height[i])
-            right_max.append(right)
-        right_max.reverse()
-        for i,j in zip(left_max,right_max):
-            min_max.append(min(i,j))
-        for i,j in zip(min_max,height):
-            drop+=(i-j)
+            right_max[i]=right
+        for i in range(len(height)):
+            min_max[i] = min(left_max[i], right_max[i])
+        for i in range(len(height)):
+            drop+=(min_max[i]-height[i])
         return drop
             
