6b39198514af6385ca6e9b810f157e71a8ef349e
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-25 13:32:30 +0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -2,44 +2,18 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result=[]
-
-        for i in range(len(nums)-2):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            left, right = i+1, len(nums)-1
-            while left<right:
-                total=nums[i]+nums[left]+nums[right]
-                if total<0:
-                    left+=1
-                elif total>0:
-                    right-=1
-                else:
-                    result.append([nums[i],nums[left],nums[right]])
-                    while left<right and nums[left]==nums[left+1]:
-                        left+=1
-                    while left<right and nums[right]==nums[right-1]:
-                        right-=1
-                    left+=1
-                    right-=1
-        return result       
-        result=set()
-        nums.sort()
-        print(nums)
-        left,right=0,len(nums)-1
         for i,value in enumerate(nums):
-            left,right=i+1,len(nums)-1
+            left=i+1
+            right=len(nums)-1
             while left<right:
-                total=nums[i]+nums[left]+nums[right]
-                if total<0:
+                total = value+nums[left]+nums[right]
+                if total==0:
+                    if (value,nums[left],nums[right]) not in result:
+                        result.append((value,nums[left],nums[right]))
                     left+=1
+                    right-=1
                 if total>0:
                     right-=1
-                if total==0:
-                    result.add(tuple([value,nums[left],nums[right]]))
+                if total<0:
                     left+=1
-                    right-=1
-        return [list(t) for t in result]
-
-
-        
-        
+        return result
