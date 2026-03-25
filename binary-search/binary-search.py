4b0daa01e11db5641f0e9804825c51c3a3a1e3d9
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-25 14:32:43 +0800

    LC-binary-search [Runtime: 55 ms; Memory: 20.5 MB]

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -10,6 +10,6 @@ class Solution:
             elif nums[mid]<target:
                 left=mid+1    
             else:
-                right=mid+1
+                right=mid-1
         return -1
 
