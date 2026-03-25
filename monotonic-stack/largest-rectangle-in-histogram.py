2f391a7c31cf77cd15c0012b260c5a6ab9416ad3
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-25 11:22:13 +0800

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- monotonic-stack/largest-rectangle-in-histogram.py
+++ monotonic-stack/largest-rectangle-in-histogram.py
@@ -2,17 +2,16 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         #很像接雨水的题目，但是接雨水的是最大，减最小的，得到能接的
         #这个像是临界的，但是取最短的，然后算面积
-        area=1*heights[0]
-        max_area=area
-        for i in range(len(heights)):
-            w=i+1
-            if len(heights)>1:
-                while w<len(heights) and heights[w]>=heights[i]:
-                    area=(w-i+1)*max(heights[i],heights[w])
-                    w+=1
-                    max_area=max(max_area,area)
-            else:
-                max_area=area
-               
+        stack = [] 
+        max_area=0
+        for i,h in enumerate(heights):
+            start=i
+            while stack and h<stack[-1][1]:
+                index,height=stack.pop()
+                max_area=max(max_area,height*(i-index))
+                start=index
+            stack.append((start,h))
+        for index,height in stack:
+            max_area=max(max_area, (len(heights)-i)*height)
         return max_area
         
