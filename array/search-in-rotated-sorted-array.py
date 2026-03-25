e4ca0eb5d4177eac1b38a6088a35e703eb4825cd
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-26 00:20:00 +0800

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array.py
+++ array/search-in-rotated-sorted-array.py
@@ -3,11 +3,21 @@ class Solution:
         left, right=0, len(nums)-1
         if target not in nums:
             return -1
-        while left<right:
-            mid = (left+right)//2   
-            if nums[mid]>target and nums[mid]>nums[right]:
-                left=mid+1
-            else:
-                right=mid
-        return left
+        
+        while left<=right:
+            mid = (left+right)//2  
+            if nums[mid]==target:
+                return mid
+            if nums[left]<=nums[mid]:
+                if nums[left]<=target<nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
             
+            else:
+                if nums[mid]<target<nums[right]:
+                    left=mid+1
+                else:
+                    right=mid-1
+        
+        return left
