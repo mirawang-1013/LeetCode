0ad7634d40c2035a6eeeba7e967a1b073652a9de
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-28 15:14:03 +0800

    LC-reverse-linked-list [Runtime: N/A; Memory: N/A]

--- linked-list/reverse-linked-list.py
+++ linked-list/reverse-linked-list.py
@@ -5,18 +5,27 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack=[]
-        if not head:
-            return None
-        curr=head
-        while curr:
-            stack.append(curr.val)
-            curr=curr.next
-        new_head=ListNode(stack.pop())
-        curr=new_head
-        while stack:
-            curr.next=ListNode(stack.pop())
-            curr=curr.next
-        return new_head
+        #双指针试试
+        p1,p2=len(head)-2,len(head)-1
+        List=[]
+        for p1,p2 in range(len(head)-1,-1,-1):
+            List.append(p2)
+            p2=p1
+
+
+        # stack 的解法
+        # stack=[]
+        # if not head:
+        #     return None
+        # curr=head
+        # while curr:
+        #     stack.append(curr.val)
+        #     curr=curr.next
+        # new_head=ListNode(stack.pop())
+        # curr=new_head
+        # while stack:
+        #     curr.next=ListNode(stack.pop())
+        #     curr=curr.next
+        # return new_head
         
         
