4cf6e8560900e6d3d7e74e1c06229c8e930c7660
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-03-29 09:00:18 +0800

    LC-merge-two-sorted-lists [Runtime: 0 ms; Memory: 19.3 MB]

--- recursion/merge-two-sorted-lists.py
+++ recursion/merge-two-sorted-lists.py
@@ -7,21 +7,62 @@ from typing import Optional, List
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode()
-        tail = dummy
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
+        dummy=ListNode(0)
+        curr=dummy
 
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-        
+        while list1 and list2:
+            if list1.val<list2.val:
+                curr.next=list1
+                list1=list1.next
+            else:
+                curr.next=list2
+                list2=list2.next
+            curr=curr.next 
+        #上面选出了下一个字符后，这句话是为了完成现有node对下一个node的指向的
+        curr.next= list1 if list1 else list2
         return dummy.next
-        
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      #history method
+        #       dummy = ListNode()
+        # tail = dummy
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         tail.next = list1
+        #         list1 = list1.next
+                
+        #     else:
+        #         tail.next = list2
+        #         list2 = list2.next
+        #     tail = tail.next
+        # if list1:
+        #     tail.next = list1
+        # else:
+        #     tail.next = list2
+        # return dummy.next
