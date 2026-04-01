c921b5e9b0aa1e953c64590889d75e00d5c8b468
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-01 14:52:27 +0800

    LC-linked-list-cycle [Runtime: 50 ms; Memory: 23 MB]

--- linked-list/linked-list-cycle.py
+++ linked-list/linked-list-cycle.py
@@ -6,14 +6,36 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited=set()
+        visited = set()
         curr=head
         while curr:
             if curr in visited:
                 return True
-            else:
-                visited.add(curr)
-                curr=curr.next
+            visited.add(curr)
+            curr=curr.next
         return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # visited=set()
+        # curr=head
+        # while curr:
+        #     if curr in visited:
+        #         return True
+        #     else:
+        #         visited.add(curr)
+        #         curr=curr.next
+        # return False
 
 #为什么必须是set()?
