efeb27fdfd99cfad6482ae8d36ed25fdaf1e512f
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-02 18:19:13 +0800

    LC-binary-search [Runtime: N/A; Memory: N/A]

--- array/binary-search.py
+++ array/binary-search.py
@@ -1,15 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right=0, len(nums)-1
-        if target not in nums:
-            return -1
-        while left<=right:
-            mid=(right+left)//2
+        left=0
+        right=len(nums)-1
+        
+        while left<right:
+            mid=(left+right)//2
             if nums[mid]==target:
                 return mid
-            elif nums[mid]<target:
-                left=mid+1    
+            elif nums[mid]>9:
+                right=mid
             else:
-                right=mid-1
+                left=mid+1
         return -1
-
+        
