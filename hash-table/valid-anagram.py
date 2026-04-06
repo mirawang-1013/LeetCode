2f73f0ca1143d59147b58467e23a9d3107883b99
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-06 23:01:11 +0800

    LC-valid-anagram [Runtime: 9 ms; Memory: 19.3 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,10 +1,15 @@
 from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        dict1={}
+        dict2={}
+        for i in s:
+            dict1[i]=dict1.get(i,0)+1
+        for j in t:
+            dict2[j]=dict2.get(j,0)+1
+        return dict1==dict2
 
-
-
+    #    return Counter(s) ==  Counter(t)
    #Solution 2     
         # pair1=[]
         # pair2=[]
