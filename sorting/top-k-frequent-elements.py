b43ecc5b22aaeca8cbfae678ccfe9f4493480d4f
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-06 23:23:01 +0800

    LC-top-k-frequent-elements [Runtime: 6 ms; Memory: 22.9 MB]

--- sorting/top-k-frequent-elements.py
+++ sorting/top-k-frequent-elements.py
@@ -5,11 +5,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         count=Counter(nums)
-        a=list(count.items())
-        output=[]
-        for i in range(k):
-            output.append(a[i][0])
-        return output
+        return [num for num,freq in count.most_common(k)]
 
         
         
