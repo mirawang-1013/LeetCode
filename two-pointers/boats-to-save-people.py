87807ced7e8c9518968cec98fc0754ffeae19986
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-08 15:33:38 +0800

    LC-boats-to-save-people [Runtime: 55 ms; Memory: 25.4 MB]

---
+++ two-pointers/boats-to-save-people.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        l,r=0,len(people)-1
+        boats=0
+        while l<=r:
+            if people[r]+people[l]<=limit:
+                l+=1
+            r-=1
+            boats+=1
+        return boats
+        
