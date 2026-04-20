a9bdd62454eef158a7d7452019694f317471a913
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-20 20:30:15 +0800

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -3,13 +3,31 @@ class Solution:
 #check if the starting point int the number set first, and then use while to loop through to see how long the consecutive string could be.
         num_set = set(nums)
         longest=0
-        for num in num_set:
-            if num-1 not in num_set:
-                current=num
+        for i in nums:
+            if i-1 not in num_set:
                 count=1
+                current=i
                 while current+1 in num_set:
-                    current+=1
                     count+=1
-                longest = max(longest,count)
-            
+                    current+=1
+                longest=max(longest,count)
         return longest
+
+
+
+
+
+
+
+        # num_set = set(nums)
+        # longest=0
+        # for num in num_set:
+        #     if num-1 not in num_set:
+        #         current=num
+        #         count=1
+        #         while current+1 in num_set:
+        #             current+=1
+        #             count+=1
+        #         longest = max(longest,count)
+            
+        # return longest
