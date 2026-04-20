a76ddac877363915eccad1e07b70872065a743c2
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-20 20:21:46 +0800

    LC-encode-and-decode-strings [Runtime: 66 ms; Memory: 19.7 MB]

--- array/encode-and-decode-strings.py
+++ array/encode-and-decode-strings.py
@@ -2,26 +2,30 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        encode=''
-        for i in strs:
-            length = len(i)
-            encode+=str(length)+'#'+i
-        return encode
-        
+        encoder=''
+        for s in strs:
+            length =str(len(s))
+            encoder+=length
+            encoder+='#'
+            encoder+=s
+        return encoder
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        res=[]
         i=0
+        decoder=[]
+        
         while i<len(s):
             j=i
-            while s[j] !='#':
+            while s[j]!='#':
                 j+=1
             length = int(s[i:j])
-            res.append(s[j+1:j+1+length])
+            decoder.append(s[j+1:j+1+length])
             i=j+1+length
-        return res
+        return decoder
+            
+
     
 
 
@@ -30,3 +34,23 @@ class Codec:
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(strs))
+
+
+#encoder
+        # encode=''
+        # for i in strs:
+        #     length = len(i)
+        #     encode+=str(length)+'#'+i
+        # return encode
+
+#decoder
+        # res=[]
+        # i=0
+        # while i<len(s):
+        #     j=i
+        #     while s[j] !='#':
+        #         j+=1
+        #     length = int(s[i:j])
+        #     res.append(s[j+1:j+1+length])
+        #     i=j+1+length
+        # return res
