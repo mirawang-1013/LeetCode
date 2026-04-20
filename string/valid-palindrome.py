6c0124b81387743d6bb373eedfc6a6cb52f2a0ec
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-21 00:05:08 +0800

    LC-valid-palindrome [Runtime: 7 ms; Memory: 24.5 MB]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,5 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        normalized = [i.lower() for i in s if i.isalnum()]
-        return normalized[::]==normalized[::-1]
+        nomalized = [i.lower() for i in s if i.isalnum()]
+        return nomalized[::]==nomalized[::-1]
+
+
+
+
+        # normalized = [i.lower() for i in s if i.isalnum()]
+        # return normalized[::]==normalized[::-1]
 
