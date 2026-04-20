bed4c9ea236088babff3907acd994c7f3a5435ac
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-20 18:19:17 +0800

    LC-two-sum [Runtime: 1 ms; Memory: 20.3 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,11 +1,38 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pairs={}
-        for i,num in enumerate(nums):
-            diff=target-num
-            if diff in pairs:
-                return [i,pairs[diff]]
-            if num not in pairs:
-                pairs[num]=i
+        pairs = {} #dict
+        for i, value in enumerate(nums):
+            gap=target-nums[i]
+            if gap in pairs:
+                return [i,pairs[gap]]
+            pairs[value]=i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # pairs={}
+        # for i,num in enumerate(nums):
+        #     diff=target-num
+        #     if diff in pairs:
+        #         return [i,pairs[diff]]
+        #     if num not in pairs:
+        #         pairs[num]=i
                 
                 
