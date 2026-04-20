95c8a25c24e115f8f55d108500f760889193eb1c
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-20 18:40:24 +0800

    LC-top-k-frequent-elements [Runtime: 7 ms; Memory: 23 MB]

--- divide-and-conquer/top-k-frequent-elements.py
+++ divide-and-conquer/top-k-frequent-elements.py
@@ -3,9 +3,22 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        count=Counter(nums)
-        return [num for num,freq in count.most_common(k)]
 
+    #方法一：heap
+        count = Counter(nums)
+        heap=[]
+        for num, freq in count.items():
+            heapq.heappush(heap,(freq, num))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        return [num for freq, num in heap]
+
+
+
+
+    #方法二：最快的方法是这个most_common 
+        # count=Counter(nums)
+        # return [num for num,freq in count.most_common(k)]
+    
         
         
