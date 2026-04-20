61d10b66bc87b6f340465052b33a55eb055fee0f
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-20 18:32:28 +0800

    LC-group-anagrams [Runtime: 13 ms; Memory: 22 MB]

--- sorting/group-anagrams.py
+++ sorting/group-anagrams.py
@@ -1,7 +1,37 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        output=defaultdict(list)
+        group = defaultdict(list) #dict
         for i in strs:
-            output[tuple(sorted(i))].append(i)
-        return list(output.values())
+            key= "".join(sorted(i))
+            group[key].append(i)
+        return list(group.values())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # output=defaultdict(list)
+        # for i in strs:
+        #     output[tuple(sorted(i))].append(i)
+        # return list(output.values())
 
