6d3818f2fc02128c6b2fcd2f4fc9130462f4b95f
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-21 18:24:39 +0800

    LC-subtree-of-another-tree [Runtime: 48 ms; Memory: 20.5 MB]

--- hash-function/subtree-of-another-tree.py
+++ hash-function/subtree-of-another-tree.py
@@ -6,21 +6,20 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSameTree(p,q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val!=q.val:
+                return False
+
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
         if not root:
             return False
-        
-        if self.isSameTree(root, subRoot):
+        if isSameTree(root, subRoot):
             return True
-
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
     
-    def isSameTree(self, s:Optional[TreeNode], t:Optional[TreeNode]) -> bool:
-        if not s and not t:
-            return True
-        if not s or not t:
-            return False
-        if s.val != t.val:
-            return False
-        
-        return self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
-        
