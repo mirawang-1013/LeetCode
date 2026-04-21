ecbe8a89da3d48bfd74285b1c9f3bdec91b0b62b
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-21 14:11:09 +0800

    LC-container-with-most-water [Runtime: 65 ms; Memory: 29.5 MB]

--- two-pointers/container-with-most-water.py
+++ two-pointers/container-with-most-water.py
@@ -3,13 +3,35 @@ class Solution:
         left,right=0,len(height)-1
         max_area=0
         while left<right:
-            high=min(height[left],height[right])
-            max_area=max(max_area,high*(right-left))
+            h=min(height[left],height[right])
+            max_area=max(max_area, h*(right-left))
             if height[left]<height[right]:
                 left+=1
             else:
                 right-=1
         return max_area
+
+
+
+
+
+
+
+
+
+
+
+
+        # left,right=0,len(height)-1
+        # max_area=0
+        # while left<right:
+        #     high=min(height[left],height[right])
+        #     max_area=max(max_area,high*(right-left))
+        #     if height[left]<height[right]:
+        #         left+=1
+        #     else:
+        #         right-=1
+        # return max_area
 
 
 
