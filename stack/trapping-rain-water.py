bd4333e2563186d3c81ccb604c9ed1a648ac23c9
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-21 14:42:44 +0800

    LC-trapping-rain-water [Runtime: 15 ms; Memory: 21.1 MB]

--- stack/trapping-rain-water.py
+++ stack/trapping-rain-water.py
@@ -2,19 +2,46 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         left=0
         right=0
+        drop=0
         left_max=[0]*len(height)
         right_max=[0]*len(height)
-        min_max=[0]*len(height)
-        drop=0
         for i in range(len(height)):
-            left=max(left,height[i])
+            left=max(height[i], left)
             left_max[i]=left
         for i in range(len(height)-1,-1,-1):
-            right=max(right,height[i])
+            right=max(height[i], right)
             right_max[i]=right
         for i in range(len(height)):
-            min_max[i] = min(left_max[i], right_max[i])
-        for i in range(len(height)):
-            drop+=(min_max[i]-height[i])
+            drop += (min(left_max[i],right_max[i])-height[i])
         return drop
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # left=0
+        # right=0
+        # left_max=[0]*len(height)
+        # right_max=[0]*len(height)
+        # min_max=[0]*len(height)
+        # drop=0
+        # for i in range(len(height)):
+        #     left=max(left,height[i])
+        #     left_max[i]=left
+        # for i in range(len(height)-1,-1,-1):
+        #     right=max(right,height[i])
+        #     right_max[i]=right
+        # for i in range(len(height)):
+        #     min_max[i] = min(left_max[i], right_max[i])
+        # for i in range(len(height)):
+        #     drop+=(min_max[i]-height[i])
+        # return drop
             
