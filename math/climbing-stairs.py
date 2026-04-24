acf6242a3ed4d7f487a6a40632d2253e7377d272
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-24 17:35:41 +0800

    LC-climbing-stairs [Runtime: 0 ms; Memory: 19.3 MB]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -1,14 +1,24 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #当小于两个台阶的时候，那就是One step可以到的事情
+#method1
         if n<=2:
             return n
         
-        #这里先用dp存在来到每一个台阶的可能性
-        dp = [0]*(n+1)
-        dp[1],dp[2] = 1,2
 
-        #写到n+1才不会只遍历到n就结束了
-        for i in range(3, n+1):
-            dp[i] = dp[i-1]+dp[i-2]
+        dp=[0]*(n+1)
+        dp[1]=1
+        dp[2]=2
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
         return dp[n]
+
+
+
+#method2 
+        # a = 1
+        # b = 2
+        # if n<=2:
+        #     return n
+        # for i in range(3,n+1):
+        #     a,b = b, a+b
+        # return b
