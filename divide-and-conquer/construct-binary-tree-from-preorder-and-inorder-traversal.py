5526548694e668b6a1767df70de80955e1bce4d0
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-04-26 22:53:58 +0800

    LC-construct-binary-tree-from-preorder-and-inorder-traversal [Runtime: 93 ms; Memory: 90.8 MB]

--- divide-and-conquer/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ divide-and-conquer/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,18 +7,13 @@
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
+        if not preorder and not inorder:
             return None
-
-        #preorder第一个肯定是根了：根-左-右
-        root_val = preorder[0] 
-        root = TreeNode(root_val) #找到根了
-
-        # 在中序中找到根节点的位置，找到根的index
-        mid=inorder.index(root_val)
-
-        # 划分左右子树,左子树等于根之前的部分，
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
         
+        val = preorder[0]
+        root=TreeNode(val)
+        mid=inorder.index(val)
+
+        root.left=self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right=self.buildTree(preorder[mid+1:],inorder[mid+1:])
         return root
