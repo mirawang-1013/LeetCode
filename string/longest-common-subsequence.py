2eb790b7c4152bd8b81daaff437f06342af029b5
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-05 15:11:39 +0800

    LC-longest-common-subsequence [Runtime: 348 ms; Memory: 44.4 MB]

--- string/longest-common-subsequence.py
+++ string/longest-common-subsequence.py
@@ -14,7 +14,6 @@ class Solution:
 
         m,n = len(text1),len(text2)
         dp=[[0]*(n+1) for _ in range(m+1)]
-
         for i in range(1,m+1):
             for j in range(1,n+1):
                 if text1[i-1]==text2[j-1]:
@@ -22,4 +21,5 @@ class Solution:
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
         return dp[m][n]
+
         
