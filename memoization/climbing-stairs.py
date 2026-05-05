b09a0506a9d4e058cbf293369b6b23c412965b07
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-05 15:08:38 +0800

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.py
+++ memoization/climbing-stairs.py
@@ -1,16 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-#method1
         if n<=2:
             return n
         dp=[0]*(n+1)
         dp[1]=1
         dp[2]=2
-        for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
+        for i in range(n+1):
+            dp[n]=dp[n-1]+dp[n-2]
         return dp[n]
-
-
 
 #method2 
         # a = 1
