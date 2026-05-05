d2d149d86f2d7d1993382619074554f3e4b5e991
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-05 15:33:36 +0800

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -3,17 +3,12 @@ class Solution:
       #if no house here
         if not nums:
             return 0
-        if len(nums) ==1:
-            return nums[0]
-
-
-        dp=[0]* len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
-
+        dp=[0]*len(num)
+        if len(num)=1:
+            return num[0]
+        if len(num)=2:
+            return max(num[0],num[1])
+        
         for i in range(2,len(nums)):
-        #不抢当前这个，那就是dp[i-1]
-        #抢当前这个，那就是dp[i-2] + nums[i]
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i]) 
-
-        return dp[-1]   
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        return dp[i-1]
