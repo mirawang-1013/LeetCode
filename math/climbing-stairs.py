ff350d1ab0699c52a80a657a2316c903fa401d30
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-05 15:09:14 +0800

    LC-climbing-stairs [Runtime: 0 ms; Memory: 19.2 MB]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -6,8 +6,8 @@ class Solution:
         dp[1]=1
         dp[2]=2
         for i in range(3,n+1):
-            dp[n]=dp[n-1]+dp[n-2]
-        return dp[n]
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[i]
 
 #method2 
         # a = 1
