2c3e90969cb45390204a533a9d644c1791edea38
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-05 14:44:10 +0800

    LC-serialize-and-deserialize-binary-tree [Runtime: 91 ms; Memory: 23.4 MB]

--- string/serialize-and-deserialize-binary-tree.py
+++ string/serialize-and-deserialize-binary-tree.py
@@ -14,17 +14,19 @@ class Codec:
         :rtype: str
         """
         def dfs(node):
-            if not node: #若没有结点就用null填充
-                vals.append("null")
-                return 
-            vals.append(str(node.val)) #把值都装进来
-            #先序遍历（根-左-右）
-            dfs(node.left) 
+            if not node:
+               vals.append("null")
+               return 
+
+            vals.append(str(node.val))
+            dfs(node.left)
             dfs(node.right)
-        
-        vals = []
+
+        vals=[]
         dfs(root)
         return ",".join(vals)
+
+
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -33,22 +35,13 @@ class Codec:
         :rtype: TreeNode
         """
         def dfs():
-            val = next(vals)
-            if val == "null":
-                return None #空结点
-            node = TreeNode(int(val)) #return一个值为val的TreeNode节点
-            node.left = dfs() #递归还原左子树，#递归还原右子树
-            node.right = dfs()
+            val=next(vals)
+            if val=="null":
+                return None
+            node=TreeNode(int(val))
+            node.left=dfs()
+            node.right=dfs()
             return node
-        vals = iter(data.split(","))
+        vals=iter(data.split(","))
         return dfs()
 
-        #the function of iter is to
-        #"1,2,null,null,3,null,null" → ["1", "2", "null", "null", "3", "null", "null"]。
-        #然后用 iter() 把它变成一个迭代器 vals，这样就可以用 next() 一个个依次取出元素
-        
-
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
