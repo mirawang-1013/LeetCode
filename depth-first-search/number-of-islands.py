d6c9dc772b1cee824a7e06c95245c5eb5073ebd5
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-07 18:26:12 +0800

    LC-number-of-islands [Runtime: 247 ms; Memory: 21.7 MB]

--- depth-first-search/number-of-islands.py
+++ depth-first-search/number-of-islands.py
@@ -2,34 +2,30 @@ from typing import List
 from collections import deque
 
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self,grid:List[List[str]])->str:
         if not grid:
             return 0
         
-        rows = len(grid)
-        cols = len(grid[0])
+        rows=len(grid)
+        columns=len(grid[0])
         count=0
 
         for i in range(rows):
-            for j in range(cols):
+            for j in range(columns):
                 if grid[i][j]=='1':
                     count+=1
-                    queue=collections.deque()
+                    queue=deque()
                     queue.append([i,j])
                     grid[i][j]='0'
-                    
-                    
                 
                     while queue:
                         x,y=queue.popleft()
                         for dx,dy in ([-1,0],[1,0],[0,1],[0,-1]):
                             nx,ny=x+dx,y+dy
-                            if 0<=nx<rows and 0<=ny<cols and grid[nx][ny]=="1":
+                            if 0<=nx<rows and 0<=ny<columns and  grid[nx][ny] == '1':
                                 queue.append([nx,ny])
-                                grid[nx][ny]=0
+                                grid[nx][ny]='0'
         return count
-                        
-                    
 
 
 
