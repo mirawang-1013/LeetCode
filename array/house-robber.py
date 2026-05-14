fd46c3ad8bf88bbb926d397951caf36cd9f78148
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-14 23:12:09 +0800

    LC-house-robber [Runtime: 0 ms; Memory: 19.4 MB]

--- array/house-robber.py
+++ array/house-robber.py
@@ -4,8 +4,7 @@ class Solution:
             return 0
         
         dp=[0]*len(nums)
-        dp[0]=nums[0]
-        
+        dp[0]=nums[0]       
         if len(nums)==1:
             return nums[0]
         dp[1]=max(nums[0],nums[1])
