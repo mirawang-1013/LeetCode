fe3992e02c54ae26ecad54ea2f2b1a63c175d18d
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-14 23:11:21 +0800

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -6,6 +6,8 @@ class Solution:
         dp=[0]*len(nums)
         dp[0]=nums[0]
         dp[1]=max(nums[0],nums[1])
+        if len(nums)==1:
+            return nums[0]
         for n in range(2,len(nums)):
             dp[n]=max(dp[n-1],dp[n-2]+nums[n])
         return dp[-1]
