763d4b5835bfa72157c7f54ca4eb3ed8bff86ea8
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-14 23:23:28 +0800

    LC-house-robber-ii [Runtime: 0 ms; Memory: 19.3 MB]

--- dynamic-programming/house-robber-ii.py
+++ dynamic-programming/house-robber-ii.py
@@ -1,19 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def rob198(nums):
+        def robInARow(nums):
             if not nums:
                 return 0
-            dp=[0]*len(nums)
-            dp[0] = nums[0]
+            dp=[0]*len(nums)  
+            dp[0]=nums[0]
             if len(nums)==1:
                 return nums[0]
             dp[1]=max(nums[0],nums[1])
-            for i in range(2,len(nums)):
-                dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+            for n in range(2,len(nums)):
+                dp[n]=max(dp[n-1],dp[n-2]+nums[n])
             return dp[-1]
-
-        if not nums: return 0
-        if len(nums) == 1: return nums[0]
-        return max(rob198(nums[:-1]),rob198(nums[1:]))
-
-        
+        if not nums:
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        return max(robInARow(nums[:-1]),robInARow(nums[1:]))
