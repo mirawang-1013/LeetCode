d6a6a92230fa8f98aa487ed726962db232dc0ad9
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-17 21:43:40 +0800

    LC-rotting-oranges [Runtime: 0 ms; Memory: 19.4 MB]

--- array/rotting-oranges.py
+++ array/rotting-oranges.py
@@ -1,30 +1,45 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        q = deque()
-        fresh = 0
+        ROWS, COLS = len(grid),len(grid[0])
+        q=deque()
+        fresh=0
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c]==2:
                     q.append((r,c))
-                elif grid[r][c] == 1:
-                    fresh +=1
-        
-        if fresh == 0: return 0
+                elif grid[r][c] ==1:
+                    fresh+=1
+        time=0
+        directions=[
+            [1,0],
+            [-1,0],
+            [0,1],
+            [0,-1]
+        ]
 
-        time = 0
-        directions = [ ( - 1 , 0 ) , ( 1 , 0 ) , ( 0 , - 1 ) , ( 0 , 1 ) ]
+        while q and fresh>0:
+            for i in range(len(q)):
+                r,c = q.popleft()
 
-        while q:
-            for _ in range(len(q)):
-                r, c = q.popleft()
                 for dr, dc in directions:
-                    nr, nc = r+dr, c+dc
-                    if 0<= nr < rows and 0<= nc <cols and grid [ nr ] [ nc ] == 1 :
-                        grid[nr][nc] = 2
-                        q.append((nr, nc))
-                        fresh -= 1
-            time +=1
-        return time - 1 if fresh == 0 else - 1
+                    row=r+dr
+                    col=c+dc
+                
+                    if (
+                        row < 0 or row >= ROWS or
+                        col < 0 or col >= COLS or
+                        grid[row][col] != 1
+                    ):
+                        continue
+                    grid[row][col] = 2
+                    fresh-=1
+                    q.append((row,col))
+            time+=1
+        if fresh==0:
+            return time
+        return -1
         
