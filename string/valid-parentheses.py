6b24975afdebef4c580cbb7aa914d0fce687746d
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-20 22:25:36 +0800

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -1,16 +1,40 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pairs = {"}":"{","]":"[",")":"("}
-        stack = []
-        for i in s: #遍历s里的括号
-            if i in pairs: #如果是右括号
-                if not stack or pairs[i]!=stack[-1]: #看stack是否为空，或者是否有左括号可以匹配
+        pairs={')':'(','}':'{',']':'['}
+        stack=[]
+        for i in s:
+            if i in pairs: #右括号
+                if not stack or pairs[i]!= stack[-1]:
                     return False
                 else:
                     stack.pop()
-            else: #如果是左括号
-                stack.append(i) 
-        return not stack
+            else:#左括号
+                stack.append(i)
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # pairs = {"}":"{","]":"[",")":"("}
+        # stack = []
+        # for i in s: #遍历s里的括号
+        #     if i in pairs: #如果是右括号
+        #         if not stack or pairs[i]!=stack[-1]: #看stack是否为空，或者是否有左括号可以匹配
+        #             return False
+        #         else: #可以匹配
+        #             stack.pop()
+        #     else: #如果是左括号
+        #         stack.append(i) 
+        # return not stack
 
 
 
