2636c738fe9a62f4a978ed067c8c232bbb78dd63
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-26 11:17:55 +0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -16,6 +16,10 @@ class Solution:
                     answer.append(ans)
                     l+=1
                     r-=1
+                    while l<r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
                 elif nums[l]+nums[r]<gap:   
                     l+=1
                 else:
