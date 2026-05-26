8d394e77996be146af4a2a490f83bce9100cfdcc
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-26 10:43:17 +0800

    LC-minimum-window-substring [Runtime: N/A; Memory: N/A]

--- sliding-window/minimum-window-substring.py
+++ sliding-window/minimum-window-substring.py
@@ -14,29 +14,31 @@ class Solution:
         #3.直到不能缩小的时候就返回字符串
         if t=="":
             return ""
-        
-        countT, window={},{}
+        countT,window={},{}
         for c in t:
             countT[c]=1+countT.get(c,0)
-        
-        have,need =0,len(countT)
-        res,resLen=[-1,-1],float("infinity")
+        have, need = 0,len(countT)
+        res=[-1,-1]
         l=0
+        resLen = float("infinity")
+
         for r in range(len(s)):
-            window[s[r]]=1+window.get(s[r],0)
-            if s[r] in countT and countT[s[r]]==window[s[r]]:
+            c=s[r]
+            window[c]=1+window.get(c,0)
+        
+            if c in countT and countT[c]==window[c]:
                 have+=1
-            
             while have==need:
-                if r - l + 1 < resLen:
-                    res=[l,r]
+                res=l,r
+                if r-l+1<resLen:
                     resLen=r-l+1
                 window[s[l]]-=1
-                if s[l] in countT and countT[s[l]]>window[s[l]]:   
-                    have-=1
+                if s[l] in countT and countT[s[l]]>window[s[l]]:
+                   have-=1
                 l+=1
         l,r=res
         return s[l:r+1] if resLen!="infinity" else ""
+
 
 
 
