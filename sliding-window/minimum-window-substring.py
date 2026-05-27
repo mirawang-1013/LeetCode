f126f9d5b250f45df105066bdc567685ae04e1b8
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-27 13:01:24 +0800

    LC-minimum-window-substring [Runtime: 59 ms; Memory: 19.6 MB]

--- sliding-window/minimum-window-substring.py
+++ sliding-window/minimum-window-substring.py
@@ -12,38 +12,30 @@ class Solution:
         #1.先计数，找到记录需要的字符和缺失的字符的办法
         #2.然后开始遍历s,等到窗口长到可以满足找到所有缺失字符时（while missing ==0)，再尝试着从左边开始缩小窗口
         #3.直到不能缩小的时候就返回字符串
-        if t=="":
-            return ""
+        if t=="":return ""
         countT,window={},{}
         for c in t:
             countT[c]=1+countT.get(c,0)
-        have, need = 0,len(countT)
-        res=[-1,-1]
+        have,need=0,len(countT)
+        res=(-1,-1)
+        resLen=float("infinity")
         l=0
-        resLen = float("infinity")
-
         for r in range(len(s)):
-            c=s[r]
-            window[c]=1+window.get(c,0)
-        
-            if c in countT and countT[c]==window[c]:
+            window[s[r]]=window.get(s[r],0)+1
+            if s[r] in countT and countT[s[r]]==window[s[r]]:
                 have+=1
             while have==need:
-                res=l,r
                 if r-l+1<resLen:
+                    res=[l,r]
                     resLen=r-l+1
                 window[s[l]]-=1
-                if s[l] in countT and countT[s[l]]>window[s[l]]:
-                   have-=1
+                if s[l]in countT and window[s[l]]<countT[s[l]]:
+                    have-=1
                 l+=1
+                       
+
         l,r=res
-        return s[l:r+1] if resLen!="infinity" else ""
-
-
-
-
-
-
+        return s[l:r+1] if resLen!=float("infinity") else ""
 
 
 
