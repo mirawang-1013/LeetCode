698e5e1ab738db97f8bb8b84ca2130e5128f84da
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-27 13:37:50 +0800

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/sliding-window-maximum.py
+++ heap-priority-queue/sliding-window-maximum.py
@@ -1,11 +1,8 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        answer=[]
-        for i in range(len(nums)-k+1):
-            new_nums=nums[i:i+k]
-            max_sum=max(new_nums)
-            answer.append(max_sum)
+        answer = []
+
+        for i in range(len(nums) - k + 1):
+            answer.append(max(nums[i:i+k]))
+
         return answer
-
-
-        
