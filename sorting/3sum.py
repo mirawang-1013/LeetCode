5d0e83e4072d89d94df8d02e47120251c1a15d7e
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-27 12:03:16 +0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -7,22 +7,20 @@ class Solution:
                 continue
             l,r=i+1,len(nums)-1
             while l<r:
-                total=nums[l]+nums[r]+v
+                total=v+nums[l]+nums[r]
                 if total==0:
-                        answer.append([v,nums[l],nums[r]])
-                        l+=1
-                        r-=1
-                        while l<r and nums[l]==nums[l-1]:
-                            l+=1
-                        while l<r and nums[r+1]==nums[r]:
-                            r-=1
-                elif total<0:
+                    answer.append([v,nums[l],nums[r]])
                     l+=1
-                else:
                     r-=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r+1]:
+                        r-=1
+                elif total>0:
+                    r-=1
+                else:
+                    l+=1
         return answer
-                    
-            
 
 
 
