f7126f24571d35871e38ac7d023db99132109635
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-27 14:37:30 +0800

    LC-valid-triangle-number [Runtime: N/A; Memory: N/A]

--- array/valid-triangle-number.py
+++ array/valid-triangle-number.py
@@ -2,8 +2,9 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         count=0
         nums.sort()
-        l=0
+        
         for i in nums[len(nums):1:-1]:
+            l=0
             r=i-1
             if nums[l]+nums[r]>i:
                 count+=r-l-1
