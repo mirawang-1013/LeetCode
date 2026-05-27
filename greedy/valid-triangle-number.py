6bccbe50d27c243ba9b982bbbe882f289d53a27f
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-27 14:41:29 +0800

    LC-valid-triangle-number [Runtime: N/A; Memory: N/A]

--- greedy/valid-triangle-number.py
+++ greedy/valid-triangle-number.py
@@ -3,15 +3,15 @@ class Solution:
         count=0
         nums.sort()
         
-        for i in nums[len(nums):1:-1]:
+        for i in range(len(nums)-1,1,-1):
             l=0
             r=i-1
-            if nums[l]+nums[r]>i:
-                count+=r-l-1
-                r-=1
-                
-            else:
-                l+=1
+            while l<r:
+                if nums[l]+nums[r]>nums[i]:
+                    count+=r-l
+                    r-=1    
+                else:
+                    l+=1
         return count
             
         
