7488fac911762eff249b5a8332dee088f728e2af
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-27 12:42:58 +0800

    LC-minimum-size-subarray-sum [Runtime: 7 ms; Memory: 30.6 MB]

--- sliding-window/minimum-size-subarray-sum.py
+++ sliding-window/minimum-size-subarray-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        nums.sort()
         answer=float("infinity")
         l=0
         sum=0
@@ -8,7 +7,7 @@ class Solution:
             sum+=nums[r]
             
             while sum>=target:
-                sum-=nums[l]
                 answer=min(answer,r-l+1)
+                sum-=nums[l]
                 l+=1
         return answer if answer!=float("infinity") else 0
