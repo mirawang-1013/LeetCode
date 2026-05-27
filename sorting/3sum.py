1715c7fa2add582c9525da04c39b0161e5322512
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-27 11:52:43 +0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -1,33 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#准备是用先排序，按次序固定住，然后再用指针去做二分查找，最后返回
+        nums.sort()
         answer=[]
-        nums.sort() #[-4,-1,-1,0,1,2]
-        gap=0
         for i,v in enumerate(nums):
-            gap=0-v #2
-            
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            l,r=i+1,len(nums)-1 #1,2
+            l,r=i+1,len(nums)-1
             while l<r:
-                if nums[l]+nums[r]==gap: #-1+2=1
-                    ans=[v,nums[l],nums[r]]
-                    answer.append(ans)
-                    l+=1
-                    r-=1
-                    #接下来两句很关键，就是要保证能一直跳过重复项
-                    while l<r and nums[l] == nums[l - 1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
-                elif nums[l]+nums[r]<gap:   
+                total=nums[l]+nums[r]+v
+                if total==0:
+                    # if [v,nums[l],nums[r]] not in answer:
+                        answer.append([v,nums[l],nums[r]])
+                        l+=1
+                        r-=1
+                        while l<r and nums[l]==nums[l-1]:
+                            l+=1
+                        while l<r and nums[r-1]==nums[r]:
+                            r-=1
+                elif total<0:
                     l+=1
                 else:
                     r-=1
         return answer
-
-                
+                    
             
 
 
