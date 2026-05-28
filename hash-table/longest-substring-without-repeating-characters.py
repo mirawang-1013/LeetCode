e2e5550d19e5bd9f7d9d02eb3e320912f93af7e7
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-05-28 18:45:29 +0800

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-without-repeating-characters.py
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -1,15 +1,52 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        subString = set()
-        Len=0
-        l=0
-        for r in range(len(s)):
-            while s[r] in subString:
-                subString.remove(s[l])
-                l+=1
-            subString.add(s[r])
-            Len = max(Len,r-l+1)
-        return Len
+        LongLeg=0
+        sub=deque()
+        for i in s:
+            while i in sub:
+                sub.popleft()
+            sub.append(i)
+            LongLeg=max(len(sub),LongLeg)
+        return LongLeg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # #当有not repeating 需求的时候都是set()
+        # substring = set()
+        # l=0
+        # L=0
+        # for r in range(len(s)):
+        #     while s[r] in substring:
+        #         substring.remove(s[l])
+        #         #remove是按照什么顺序在remove?
+        #         l+=1
+        #     substring.add(s[r])          
+        #     L=max(L,r-l+1)
+        #     #取最长的时候一般都会用到这个max()的函数
+        # return L
 
 
 
