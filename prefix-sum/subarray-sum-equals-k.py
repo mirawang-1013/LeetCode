febd48c0210091ce5e55b6c9d30a0ab1e8ede8ae
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-06-02 19:34:09 +0800

    LC-subarray-sum-equals-k [Runtime: 36 ms; Memory: 23.5 MB]

--- prefix-sum/subarray-sum-equals-k.py
+++ prefix-sum/subarray-sum-equals-k.py
@@ -4,14 +4,12 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         prefix_count=defaultdict(int)
         prefix_count[0]=1
-
-        prefix=0 #加总的和
-        ans=0 #最后返回的答案
+        prefix=0
+        ans=0
 
         for num in nums:
             prefix+=num
             ans+=prefix_count[prefix-k]
             prefix_count[prefix]+=1
         return ans
-
         
