cb715487b8ae3eba4e0798e36bdc2d075fefa623
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-06-04 10:17:36 +0800

    LC-reverse-linked-list [Runtime: 0 ms; Memory: 20.5 MB]

--- recursion/reverse-linked-list.py
+++ recursion/reverse-linked-list.py
@@ -5,12 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #双指针试试
-        p1,p2=len(head)-2,len(head)-1
-        List=[]
-        for p1,p2 in range(len(head)-1,-1,-1):
-            List.append(p2)
-            p2=p1
+        prev=None
+        curr=head
+
+        while curr:
+            nxt=curr.next
+            curr.next=prev
+            
+            prev=curr
+            curr=nxt
+        return prev
+
 
 
         # stack 的解法
