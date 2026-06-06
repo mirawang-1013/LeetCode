b7920b65ac48185f16130a9796e8e2c7b92df15a
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-06-06 22:41:29 +0800

    LC-sort-an-array [Runtime: 771 ms; Memory: 26.6 MB]

---
+++ divide-and-conquer/sort-an-array.py
@@ -0,0 +1,41 @@
+from typing import List
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        def merge(arr,L,M,R):
+            left=arr[L:M+1]
+            right=arr[M+1:R+1]
+
+            i,j,k=L,0,0
+
+            while j<len(left) and k<len(right):
+                if left[j]<=right[k]:
+                    arr[i]=left[j]
+                    j+=1
+                else:
+                    arr[i]=right[k]
+                    k+=1
+                i+=1
+            
+            while j<len(left):
+                arr[i]=left[j]
+                j+=1
+                i+=1
+            
+            while k<len(right):
+                arr[i]=right[k]
+                k+=1
+                i+=1
+        
+        def merge_sort(arr,L,R):
+            if L>=R:
+                return 
+            M=(L+R)//2
+            merge_sort(arr,L,M)
+            merge_sort(arr,M+1,R)
+
+            merge(arr,L,M,R)
+        
+        merge_sort(nums,0,len(nums)-1)
+        return nums
+        
