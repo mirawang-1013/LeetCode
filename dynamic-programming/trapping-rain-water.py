6365bf3a4702d764f776ba324f8dc693aa334457
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-06-25 16:33:48 +0800

    LC-trapping-rain-water [Runtime: 11 ms; Memory: 20.8 MB]

--- dynamic-programming/trapping-rain-water.py
+++ dynamic-programming/trapping-rain-water.py
@@ -1,20 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left=0
-        right=0
-        drop=0
+        left,right,drop=0,0,0
         left_max=[0]*len(height)
         right_max=[0]*len(height)
         for i in range(len(height)):
-            left=max(height[i], left)
+            left=max(left,height[i])
             left_max[i]=left
         for i in range(len(height)-1,-1,-1):
-            right=max(height[i], right)
+            right=max(right,height[i])
             right_max[i]=right
         for i in range(len(height)):
-            drop += (min(left_max[i],right_max[i])-height[i])
+            drop+=(min(left_max[i],right_max[i])-height[i])
         return drop
-
 
 
 
