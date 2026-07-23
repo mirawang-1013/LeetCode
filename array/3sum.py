93f279df7f606c3f451e96cf2d7ecf7c65602073
MiraWang <67632143+mirawang-1013@users.noreply.github.com>
2026-07-23 17:14:08 +0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -1,15 +1,16 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        answer=[]
-        for i,v in enumerate(nums):
+        result=[]
+        for i in range(len(nums)):
+            
             if i>0 and nums[i]==nums[i-1]:
                 continue
-            l,r=i+1,len(nums)-1
+            l,r = i+1, len(nums)-1
             while l<r:
-                total=v+nums[l]+nums[r]
-                if total==0:
-                    answer.append([v,nums[l],nums[r]])
+                total = nums[i]+nums[l]+nums[r]
+                if total == 0:
+                    result.append([nums[i],nums[l],nums[r]])
                     l+=1
                     r-=1
                     while l<r and nums[l]==nums[l-1]:
@@ -18,9 +19,51 @@ class Solution:
                         r-=1
                 elif total>0:
                     r-=1
-                else:
+                elif total<0:
                     l+=1
-        return answer
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # nums.sort()
+        # answer=[]
+        # for i,v in enumerate(nums):
+        #     if i>0 and nums[i]==nums[i-1]:
+        #         continue
+        #     l,r=i+1,len(nums)-1
+        #     while l<r:
+        #         total=v+nums[l]+nums[r]
+        #         if total==0:
+        #             answer.append([v,nums[l],nums[r]])
+        #             l+=1
+        #             r-=1
+        #             while l<r and nums[l]==nums[l-1]:
+        #                 l+=1
+        #             while l<r and nums[r]==nums[r+1]:
+        #                 r-=1
+        #         elif total>0:
+        #             r-=1
+        #         else:
+        #             l+=1
+        # return answer
 
 
 
